13fe4d5573761b2c6e6335b79318a6a59cbae3fa
Sarah Harvey <SarahHarvey@users.noreply.github.com>
2025-03-05 23:46:11 -0500

    adding jacobian functionality

--- jsutils.py
+++ jsutils.py
@@ -5,7 +5,7 @@ Jacobian similarity helper functions.
 import torch
 import warnings
 import numpy as np
-
+import metrics
 
 def decoding_jacobian(x1, model_2nd_half):
     Js = []
@@ -35,3 +35,41 @@ def convert_Jacobian(J_dict, model_name):
         Js.append(J_mbyn)
         print(j)
     return Js
+
+
+
+def compute_Jacobian_Bures_distances(J_dict, model_names):
+    """
+    Computes a matrix of Bures distances between the Jacobians given in a dictionary J_dict  
+
+    Parameters
+    ----------
+    J_dict : Dictionary of Jacobians associated with each model.  Keys are model names.  Each value is a list of torch tensors extracted by the function decoding_jacobian().    
+
+    model_names:  list of strings.
+        List of models to compute the distance between, and the order they will be listed in in the distance matrix.
+
+    Returns
+    -------
+    bures_dists_all : list
+        List of bures distance matrices.  The ith distance matrix is the distances among the models' ith Jacobians in their respective lists of Jacobians.
+    """
+    bures_dists_all = []
+    
+    for outer_ind in range(len(J_dict[model_names[0]])):
+        bures_dists =  np.zeros((len(model_names),len(model_names)))
+    
+        for i in range(len(model_names)):
+            for j in range(len(model_names)):
+                if j < i:
+                    JX = J_dict[model_names[i]][outer_ind].numpy()
+                    JY = J_dict[model_names[j]][outer_ind].numpy()
+                    bures_dists[i,j] = metrics.sq_bures_metric(JX,JY)
+    
+        bures_dists = bures_dists + bures_dists.T
+        bures_dists_all_rbyn.append(bures_dists)
+    
+        print(str(outer_ind) + "/" + str(len(J_dict[model_names[0]])) )
+
+    return bures_dists_all
+
